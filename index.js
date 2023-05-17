0b9652738cccad637c9944ca2074c298ac354e9d
roki4 <mark.gavrilov.2018@list.ru>
2023-05-17 22:20:41 +0300

    fix path

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const TelegramBot = require('node-telegram-bot-api');
 const config = require('./config/index');
-const database = require('./database/index.js');
+const database = require('./src/database/index');
 const Film = database.model('Film'); // подключаем наш образец модели
 const Cinema = database.model('Cinema');
 const User = database.model('User');
